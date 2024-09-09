1f8a01dcf7d0175711e3839307c4e97777f0ec3b
Madiha Fatima <144526926+madihamalghani@users.noreply.github.com>
2024-09-09 22:08:17 +0500

    Add files via upload

---
+++ basicConceptNotes.py
@@ -0,0 +1,34 @@
+print('compiling first python code 9 September 2024')
+# input in python: (by default in string form)
+print('Enter your name:')
+x = input() 
+print('Hello, ' + x)
+#input in integer/float
+y = int(input('enter your age '))
+# Correctly concatenating the integer with a string by converting the integer to a string
+print('your age is ' +  str(y) + ' years old')
+# --------------------------------------------------------------
+            # Arithmetic Operations in pythons:
+# --------------------------------------------------------------
+# Addition:
+a=int(input('input first number '))
+b=int(input('input second number '))
+sum=a+b
+print('Sum is ' + str(sum))
+# Subtraction:
+sub=a-b
+print('Subtraction is ' + str(sub))
+# Multiplication:
+multiplication=a*b
+print('Multiplication is ' + str(multiplication))
+# Division:
+division=a/b
+print('Division is ' + str(division))
+# module:(gives quotient)
+module=a//b
+print('Module is ' + str(module))
+# percentage:(gives remainder)
+remainder=a%b
+print('Remainder is ' + str(remainder))
+
+
